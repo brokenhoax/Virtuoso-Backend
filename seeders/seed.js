fc0659fa253ec6cd8a91a492ddee79fb9c107aa6
Juan Osorio <juan.michael.osorio@gmail.com>
2020-08-28 16:24:43 -0500

    maping data for seeding

--- seeders/seed.js
+++ seeders/seed.js
@@ -9,42 +9,43 @@ mongoose.connect("mongodb://localhost/projectdb", {
   useUnifiedTopology: true
 });
 
+const users = ["Bill", "Phillip", "Edward", "Pablo", "Gil", "Francesca", "Edith", "Sasha"];
+let UserSeed = [];
+UserSeed = users.map(user => ({
+  username: user,
+  password: "password",
+  role: "Virtuoso"
+})
+);
 
-
-const UserSeed = [
-  {
-    username: 'bill',
-    password: "pasword",
-    role: "Virtuoso"
-  },
-  {
-    username: 'bing',
-    password: "pasword",
-    role: "Virtuoso"
+const webinars = ["Data and you!", "Best Financing Practices", "Putting the We in Web dev!", "How to get a raise in 10 days!", "What to do when you become the boss", "Uh Oh, my database has been wiped!"]
+let WebinarSeed = [];
+WebinarSeed = webinars.map(webinar => ({
+  title: webinar,
+  description: "This is a really awesome Webinar!!!",
+  date: new Date,
+  hosts: "Bill and Ted",
+  duration: "06:30",
+  mainTopic: "Webinar",
+  skillLevel: "Beginner",
+  tags: {
+    educational: true,
+    networking: true,
+    finance: true,
+    marketing: false,
+    engineering: false
   }
-
-];
-
-const WebinarSeed = [
-  {
-    title: "test",
-    skillLevel: "Beginner"
-  },
-  {
-    title: "test",
-    skillLevel: "Beginner"
-  }
-
-];
+})
+);
 
 
 db.User.deleteMany({})
-  .then(() => { return db.Webinar.deleteMany({}) }
-  )
+  .then(() => {
+    return db.Webinar.deleteMany({})
+  })
   .then(() => {
     return db.User.insertMany(UserSeed);
-  }
-  )
+  })
   .then(() => {
     return db.Webinar.insertMany(WebinarSeed);
   })
