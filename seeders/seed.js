1367a32a778ee89214dfffd671f28e8e8e0a52d3
Juan Osorio <juan.michael.osorio@gmail.com>
2020-08-28 17:16:16 -0500

    Created randomizers for role and skill to diversify data

--- seeders/seed.js
+++ seeders/seed.js
@@ -11,15 +11,37 @@ mongoose.connect("mongodb://localhost/projectdb", {
 
 const users = ["Bill", "Phillip", "Edward", "Pablo", "Gil", "Francesca", "Edith", "Sasha"];
 let UserSeed = [];
+
+const role = () => {
+  const biNum = (Math.floor(Math.random() * 2) + 1)
+  if (biNum === 1) {
+    return "Virtuoso";
+  } else {
+    return "Prodigy";
+  }
+}
+
 UserSeed = users.map(user => ({
   username: user,
   password: "password",
-  role: "Virtuoso"
+  role: role()
 })
 );
 
 const webinars = ["Data and you!", "Best Financing Practices", "Putting the We in Web dev!", "How to get a raise in 10 days!", "What to do when you become the boss", "Uh Oh, my database has been wiped!"]
 let WebinarSeed = [];
+
+const skill = () => {
+  const triNum = (Math.floor(Math.random() * 3) + 1);
+  if (triNum === 1) {
+    return "Beginner";
+  } else if (triNum === 2) {
+    return "Intermediate"
+  } else {
+    return "Advanced";
+  }
+}
+
 WebinarSeed = webinars.map(webinar => ({
   title: webinar,
   description: "This is a really awesome Webinar!!!",
@@ -27,7 +49,7 @@ WebinarSeed = webinars.map(webinar => ({
   hosts: "Bill and Ted",
   duration: "06:30",
   mainTopic: "Webinar",
-  skillLevel: "Beginner",
+  skillLevel: skill(),
   tags: {
     educational: true,
     networking: true,
