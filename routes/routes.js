02077a01d04a6e834ece52923fbdc779650da015
Matt <matthewjmcivor@gmail.com>
2020-08-29 12:45:49 -0500

    Fixed some minor issues before Pull Req

--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,18 @@
 const router = require("express").Router();
 
 const WebControl = require('../controllers/webinarController');
+const UserControl = require('../controllers/userController');
 
 router.post('/webinar/create', WebControl.create);
 router.patch('/webinar/update/:id', WebControl.update);
 router.delete('/webinar/delete/:id', WebControl.delete);
 router.get('/webinar/get/id/:id', WebControl.getId);
 router.get('/webinar/get/all', WebControl.getAll);
+
+router.post('/user/create', UserControl.create);
+router.patch('/user/update/:id', UserControl.update);
+router.delete('/user/delete/:id', UserControl.delete);
+router.get('/user/get/id/:id', UserControl.getId);
+router.get('/user/get/all', UserControl.getAll);
 
 module.exports = router;
