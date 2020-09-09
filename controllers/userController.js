6d669fc906b237eb8f13c9953a6f585c2ccb9e7c
Juan Osorio <juan.michael.osorio@gmail.com>
2020-09-08 23:21:38 -0500

    added routes for population and updated seed

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,7 @@
 const userAuth = require("./functions/UserAuth.js");
 const db = require("../models");
 
-exports.create = async ({body}, res) => {
+exports.create = async ({ body }, res) => {
     if (!userAuth.exists(body)) {
         return res.status(400).json({ success: false, error: "No body or null body received." }).end()
     }
@@ -16,25 +16,25 @@ exports.create = async ({body}, res) => {
         return res.status(409).json({ success: false, error: "Email already exists" }).end();
     } else {
         User
-        .save()
-        .then(() => {
-            return res.status(201).json({
-                success: true,
-                id: User._id,
-                message: 'User created!',
-            }).end()
-        })
-        .catch(error => {
-            return res.status(400).json({
-                error,
-                message: 'User not created!',
-            }).end()
-        })
+            .save()
+            .then(() => {
+                return res.status(201).json({
+                    success: true,
+                    id: User._id,
+                    message: 'User created!',
+                }).end()
+            })
+            .catch(error => {
+                return res.status(400).json({
+                    error,
+                    message: 'User not created!',
+                }).end()
+            })
     }
 }
 
-exports.update = async (req,res) => {
-    const {body} = req;
+exports.update = async (req, res) => {
+    const { body } = req;
     if (!userAuth.exists(body)) {
         return res.status(400).json({ success: false, error: "No body or null body received." }).end()
     }
@@ -45,7 +45,7 @@ exports.update = async (req,res) => {
             error: 'You must provide all the required information for the form!'
         }).end()
     }
-    
+
     db.User.findOne({ _id: User.id }, (err, user) => {
         if (err) {
             return res.status(404).json({
@@ -77,7 +77,7 @@ exports.update = async (req,res) => {
     })
 }
 
-exports.delete = async (req,res) => {
+exports.delete = async (req, res) => {
     await db.User.findOneAndDelete({ _id: req.params.id }, (err, user) => {
         if (err) {
             return res.status(400).json({ success: false, error: err }).end()
@@ -93,7 +93,7 @@ exports.delete = async (req,res) => {
     }).catch(err => console.log(err))
 }
 
-exports.getId = async (req,res) => {
+exports.getId = async (req, res) => {
     await db.User.findOne({ _id: req.params.id }, (err, user) => {
         if (err) {
             return res.status(400).json({ success: false, error: err }).end()
@@ -108,7 +108,7 @@ exports.getId = async (req,res) => {
     }).catch(err => console.log(err))
 }
 
-exports.getAll = async (req,res) => {
+exports.getAll = async (req, res) => {
     await db.User.find({}, (err, user) => {
         if (err) {
             return res.status(400).json({ success: false, error: err })
@@ -122,7 +122,7 @@ exports.getAll = async (req,res) => {
     }).catch(err => console.log(err))
 }
 
-exports.verifyUser = async ({body},res) => {
+exports.verifyUser = async ({ body }, res) => {
 
     userAuth.verifyBody(body);
     const User = new db.User(body);
@@ -140,4 +140,64 @@ exports.verifyUser = async ({body},res) => {
         }
         return res.status(200).json({ success: true, data: user }).end()
     }).catch(err => console.log(err))
+}
+
+exports.getUserFavorite = async (req, res) => {
+    await db.User.findOne({ _id: req.params.id })
+        .populate("favorite")
+        .then((userFavoriteWebinar, err) => {
+            if (err) {
+                return res.status(400).json({ success: false, error: err })
+            }
+            if (!userFavoriteWebinar) {
+                return res.status(404)
+                    .json({ success: false, error: `User's favorite webinars not found` }).end()
+            }
+            return res.status(200).json({ success: true, data: userFavoriteWebinar }).end()
+        }).catch(err => console.log(err))
+}
+
+exports.getUserRegistered = async (req, res) => {
+    await db.User.findOne({ _id: req.params.id })
+        .populate("registered")
+        .then((userRegisteredWebinar, err) => {
+            if (err) {
+                return res.status(400).json({ success: false, error: err })
+            }
+            if (!userRegisteredWebinar) {
+                return res.status(404)
+                    .json({ success: false, error: `User's registered webinars not found` }).end()
+            }
+            return res.status(200).json({ success: true, data: userRegisteredWebinar }).end()
+        }).catch(err => console.log(err))
+}
+
+exports.getUserCompleted = async (req, res) => {
+    await db.User.findOne({ _id: req.params.id })
+        .populate("completedVideo")
+        .then((userCompletedWebinar, err) => {
+            if (err) {
+                return res.status(400).json({ success: false, error: err })
+            }
+            if (!userCompletedWebinar) {
+                return res.status(404)
+                    .json({ success: false, error: `User's completed webinars not found` }).end()
+            }
+            return res.status(200).json({ success: true, data: userCompletedWebinar }).end()
+        }).catch(err => console.log(err))
+}
+
+exports.getUserPassed = async (req, res) => {
+    await db.User.findOne({ _id: req.params.id })
+        .populate("passedQuiz")
+        .then((userPassedWebinar, err) => {
+            if (err) {
+                return res.status(400).json({ success: false, error: err })
+            }
+            if (!userPassedWebinar) {
+                return res.status(404)
+                    .json({ success: false, error: `User's passed quizes not found` }).end()
+            }
+            return res.status(200).json({ success: true, data: userPassedWebinar }).end()
+        }).catch(err => console.log(err))
 }
