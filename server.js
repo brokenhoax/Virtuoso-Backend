9663332540d920eacafe7033c641bd58a17bcbbf
Matt <matthewjmcivor@gmail.com>
2020-09-10 16:33:02 -0500

    Patch for Heroku Deployment (dotENV setup)

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const routes = require("./routes/routes");
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -12,6 +12,6 @@ app.use(express.json());
 app.use(cors());
 app.use(routes);
 
-mongoose.connect("mongodb://localhost/projectdb", {useNewUrlParser: true});
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true});
 
 app.listen(PORT, () => {console.log(`App is listening on Port: ${PORT}`)});
